dd9eb841037b1880722e02036959ce33a8bde09b
Ayaanfaraz <35631129+Ayaanfaraz@users.noreply.github.com>
2019-06-01 10:50:08 -0500

    Add files via upload

--- final.py
+++ final.py
@@ -4,20 +4,22 @@ from urllib import urlopen
 stats = urlopen('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=1151FF2B5E80B68F9951145B6297FD2E&steamid=76561198230401688').read()
 
 
-def getStat(statName):
+def getStat(statName, stats):
     test = str(stats)
     array = test.split('"');
     index = array.index(statName)
     valueIndex = index + 3
     total = array[valueIndex]
-    ktotal = str(total)
-    totalC = str(re.findall('\d+', ktotal)[0])
-    kI = float(totalC)
-    return kI
+    stattotal = str(total)
+    totalC = str(re.findall('\d+', stattotal)[0])
+    finalTotal = float(totalC)
+    return finalTotal
 
-kills = getStat("total_kills")
-deaths = getStat("total_deaths")
+kills = getStat("total_kills", stats)
+deaths = getStat("total_deaths", stats)
+totalContribution = getStat("total_contribution_score", stats)
 
-print("Total Kills: ", int(kills))
-print("Total Deaths: ", int(deaths))
+#print("Total Kills: ", int(kills))
+#print("Total Deaths: ", int(deaths))
 print("K/D Ratio: ", float(kills/deaths))
+print("Contribution Score: ", int(totalContribution))
