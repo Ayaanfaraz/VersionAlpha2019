9a42d452c95abfa01af2647cc6281ad632930da9
Ayaanfaraz <35631129+Ayaanfaraz@users.noreply.github.com>
2019-06-01 11:24:09 -0500

    Add files via upload

--- final.py
+++ final.py
@@ -1,7 +1,14 @@
 
 import re
+
+def urlString(apiKEY, steamID):
+    return 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key='+apiKEY+'&steamid='+steamID
+
+apiK = str(1151FF2B5E80B68F9951145B6297FD2E)
+steamI = 76561198230401688
+
 from urllib import urlopen
-stats = urlopen('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=1151FF2B5E80B68F9951145B6297FD2E&steamid=76561198230401688').read()
+stats = urlopen(urlString(apiK, steamI)).read()
 
 
 def getStat(statName, stats):
@@ -21,5 +28,8 @@ totalContribution = getStat("total_contribution_score", stats)
 
 #print("Total Kills: ", int(kills))
 #print("Total Deaths: ", int(deaths))
+
 print("K/D Ratio: ", float(kills/deaths))
 print("Contribution Score: ", int(totalContribution))
+
+f= open("initial.txt","w+")
